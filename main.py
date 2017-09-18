30f1fc2e1adde279a6337a95db68a935e0d79833
Josh Parkinson <parkinsonxc@gmail.com>
2017-09-18 18:54:51 -0500

    Added form variable

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, requests
+
+app = Flast(__name__)
+app.config['DEBUG'] = True
+
+
+form = """
+    <html>
+        <head>
+            <style>
+                form {
+                    background-color: #eee;
+                    padding: 20px;
+                    margin: 0 auto;
+                    width: 540px;
+                    font: 16px sans-serif;
+                    border-radius: 10px;
+                }
+
+                textarea {
+                    margin: 10px 0;
+                    width: 540px;
+                    height: 120px;
+                }
+            </style>
+        </head>
+        <body>
+            <!--create your form here -->
+        </body>
+    </html>
+"""
+
+
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+app.run()
